2b3479fa94a762ed1b6c82e500fd6d60e3756682
Carsen Ball <ballcarsen@gmail.com>
2019-02-04 18:54:16 -0700

    I think I completed the grammar file, it generates the lexer and parser but I don't know how to get the output file.

---
+++ compile/ParserMain.py
@@ -0,0 +1,18 @@
+import sys
+from antlr4 import *
+from TinyLexer import TinyLexer
+from TinyParser import TinyParser
+
+
+def main(argv):
+    input = FileStream(argv[1])
+    lexer = TinyLexer(input)
+    stream = CommonTokenStream(lexer)
+    parser = TinyParser(stream)
+    tree = parser.start()
+    printer = KeyPrinter()
+    walker = ParseTreeWalker()
+    print(tree.toStringTree(recog=parser))
+
+if __name__ == '__main__':
+    main(sys.argv)
