99bf9a698bfb96831d1c16a8f56549e53700966a
Carsen Ball <ballcarsen@gmail.com>
2019-03-11 16:33:10 -0600

    Added venv capability, changed targed of antlr download to the environment Library.

--- compile/Micro.sh
+++ compile/Micro.sh
@@ -3,9 +3,9 @@
 python3 -m venv antlr-env
 source antlr-env/bin/activate
 pip3 install antlr4-python3-runtime
-cd antlr-env/lib/
+cd antlr-env/bin/
 curl -O https://www.antlr.org/download/antlr-4.7.1-complete.jar
 cd ../../
 
-eval 'java -jar /antlr-env/bin/lib/antlr-4.7.2-complete.jar -Dlanguage=Python3 Tiny.g4'
+eval 'java -jar /antlr-env/bin/antlr-4.7.2-complete.jar -Dlanguage=Python3 Tiny.g4'
 ./Scanner.py $1
