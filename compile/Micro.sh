ec6188b6b04a9fd41147b7bcf46cbedd49c0f828
Kirby <knoverman@comcast.net>
2019-03-11 16:33:19 -0600

    updated Micro

--- compile/Micro.sh
+++ compile/Micro.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+eval 'wget https://www.antlr.org/download/antlr-4.7.2-complete.jar'
 eval 'java -jar /usr/local/lib/antlr-4.7.2-complete.jar -Dlanguage=Python3 Tiny.g4'
 ./Scanner.py $1
