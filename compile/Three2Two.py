3438b03388a8558921a37ad5a9b68a22abc605ea
Carsen Ball <ballcarsen@gmail.com>
2019-04-21 22:11:25 -0600

    fixed sub again

--- compile/Three2Two.py
+++ compile/Three2Two.py
@@ -6,37 +6,28 @@ def convert(lines):
     for index in range(len(lines)):
         adjust = [True for oper in flagOpers if oper in lines[index]]
         if 'subi' in lines[index]:
-            top_line = lines[index-2].split(" ")
-            f_line = lines[index-1].split(" ")
-            line = lines[index].split(" ")
-            n_line = lines[index + 1].split(" ")
+            line = lines[index].split(' ')[:-1]
+            next_line = lines[index + 1].split(' ')
+            tmp1 = line[1]
+            tmp2 = line[2]
+            line[1] = tmp2
+            line[2] = tmp1
 
-            reg = f_line[2]
-            tmp = top_line[2]
-            f_line[2] = tmp
-            top_line[2] = reg
-            line = line[:-1]
-            n_line[1] = tmp
-            f_line = " ".join(x for x in f_line)
-            top_line = " ".join(x for x in top_line)
+            next_line[1] = tmp1
+
             line = " ".join(x for x in line)
-            n_line = " ".join(x for x in n_line)
+            next_line = " ".join(x for x in next_line)
 
-            lines[index - 2]= top_line
-            lines[index - 1] = f_line
             lines[index] = line
-            lines[index + 1] = n_line
+            lines[index + 1] = next_line
         elif True in adjust:
-            f_line = lines[index-2].split(" ")
             line = lines[index].split(" ")
             n_line = lines[index + 1].split(" ")
-            reg = f_line[2]
+            reg = line[3]
             line = line[:-1]
             n_line[1] = reg
-            f_line = " ".join(x for x in f_line)
             line = " ".join(x for x in line)
             n_line = " ".join(x for x in n_line)
-            lines[index - 2]= f_line
             lines[index] = line
             lines[index + 1] = n_line
     for l in lines:
